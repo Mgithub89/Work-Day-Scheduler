59680074c62fe90362e0a78e9364274370711898
metages worku <metages09@gmail.com>
2020-10-05 20:00:53 -0700

    add comment in script.js code and update readme.md

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Work-Day-Scheduler
-  * A simple calendar application that allows the user to save events for each standard business hour of the day.
+  * A simple daily planner application that allows the user to save relevant events for each standard business hour of the day to manage time effectively.
 
 Explore the [project-page](https://github.com/Mgithub89/Work-Day-Scheduler.git)
 
@@ -13,18 +13,18 @@ View the [deployed-app](https://mgithub89.github.io/Work-Day-Scheduler/)
 
  ## About The Project 
   
-  The objective of this project is to create a simple calendar application that allows a user to save events for each standard working hour of the day based on a user acceptance criteria.
-  This app displays current day at the top of the callendar by using **Moment.js** and allows a user to create a schedule for each colored time block that indicates whether it is in the past(grey colored), present(red colored) or future(green colored). The user can also enter an event and save in local storage by clicking the save button.
+  The objective of this project is to create a simple daily planner application that allows a user to save events for each standard business hour of the day based on a user acceptance criteria.
+  This app displays current day at the top of the callendar by using **Moment.js** and allows a user to create a schedule for each colored time block that indicates whether it is in the past(grey colored), present(red colored) or future(green colored) based on time of the day. The user can also save each time block input text  in local storage by clicking the save button.
 
 ## Mock-Up
 ![Ui](Capture.PNG)
 
 ## Built With 
-   * HTML
-   * CSS
-   * JavaScript/JQuery
-   * Moment.js
-   * Bootstrap
+   *  HTML
+   *  Bootstrap - CSS Framework
+   *  Jquery - JavaScript Library
+   *  Moment.js - JavaScript Library
+    
 
 ## Contact
 Metages Worku - [metages09@gmail.com](mailto:metages09@gmail.com)
--- script.js
+++ script.js
@@ -1,72 +1,56 @@
-var currentDate = moment()
 
-//add current date and time 
-$("#currentDay").text(currentDate.format("llll"));
+$(document).ready(function () {
 
-let currentTime = currentDate.format("H");
+    //displaying current date and time using moment.js
+    var currentDate = moment()
+    $("#currentDay").text(currentDate.format("llll"));
 
-//comparing currentTime with data-number of each time block element
-$(".form-control").each(function () {
-    // add class "present" if currentTime equals data number
-    if ($(this).data("number") == currentTime) {
+    let currentTime = currentDate.format("H");
 
-        $(this).addClass("present");
-    }
-    // add class "past" if data-number less than currentTime
-    else if ($(this).data("number") < currentTime) {
+    // loop to compare currentTime with data-number of each time block element and change the color
+    $(".form-control").each(function () {
+        // add class "present" if currentTime equals data number
+        if ($(this).data("number") == currentTime) {
 
-        $(this).addClass("past");
-    }
-    // add class "future" if data-number greater than currentTime
-    else {
-        $(this).addClass("future");
-    }
-});
-
-$(".saveBtn").on("click", function () {
-
-    // localStorage.setItem("9AM", ($("#9AM").val()))
-    // localStorage.setItem("10AM", ($("#10AM").val()))
-    // localStorage.setItem("11AM", ($("#11AM").val()))
-    // localStorage.setItem("12PM", ($("#12PM").val()))
-    // localStorage.setItem("1PM", ($("#1PM").val()))
-    // localStorage.setItem("2PM", ($("#2PM").val()))
-    // localStorage.setItem("3PM", ($("#3PM").val()))
-    // localStorage.setItem("4PM", ($("#4PM").val()))
-    // localStorage.setItem("5PM", ($("#5PM").val()))
-
-    var appoText = $(this).parent('div').children('.inputText').children('textarea').val();
-    var appoTime = $(this).parent("div").children(".time-block").text();
-
-    if (appoText === "") {
-        return
-    }
-    else {
-        localStorage.setItem(appoTime, appoText);
-    }
-
-});
-
-// $("#9AM").text(localStorage.getItem("9AM"));
-// $("#10AM").text(localStorage.getItem("10AM"));
-// $("#11AM").text(localStorage.getItem("11AM"));
-// $("#12PM").text(localStorage.getItem("12PM"));
-// $("#1PM").text(localStorage.getItem("1PM"));
-// $("#2PM").text(localStorage.getItem("2PM"));
-// $("#3PM").text(localStorage.getItem("3PM"));
-// $("#4PM").text(localStorage.getItem("4PM"));
-// $("#5PM").text(localStorage.getItem("5PM"));
-
-
-
-$("textarea").each(function () {
-    for (i = 0; i < localStorage.length; i++) {
-        var id = $(this).attr("id");
-        var key = localStorage.key(i);
-        var text = localStorage.getItem(key);
-
-        if (id === key) {
-            $(this).text(text)
+            $(this).addClass("present");
         }
-    }
-})
+        // add class "past" if data-number less than currentTime
+        else if ($(this).data("number") < currentTime) {
+
+            $(this).addClass("past");
+        }
+        // add class "future" if data-number greater than currentTime
+        else {
+            $(this).addClass("future");
+        }
+    });
+
+    //saving value to the local storage using onclick function
+    $(".saveBtn").on("click", function (event) {
+        event.preventDefault();
+        var eventText = $(this).parent('div').children('.inputText').children('textarea').val();
+        var eventTime = $(this).parent("div").children(".time-block").text();
+        //if event text null return
+        if (eventText === "") {
+            return
+        }
+        // save the event in localstorage(key - value pair)
+        else {
+            localStorage.setItem(eventTime, eventText);
+        }
+
+    });
+    //loop over textarea and set the localstorage value into textarea
+    // saved event persist.  
+    $("textarea").each(function () {
+        for (i = 0; i < localStorage.length; i++) {
+            var eventId = $(this).attr("id");
+            var eventKey = localStorage.key(i);
+            var text = localStorage.getItem(eventKey);
+            //set the value to textarea if eventid===eventkey
+            if (eventId === eventKey) {
+                $(this).text(text)
+            }
+        }
+    })
+});
